39fe009b0bf02f106b9aa051d6d257c62aaaa2f5
Ericcentrik <enaiman1@gmail.com>
2020-03-31 19:22:32 -0400

    fixed another bug

--- server/api/book/book.controller.js
+++ server/api/book/book.controller.js
@@ -15,7 +15,7 @@ exports.allBooks = (req, res) => {
 exports.saveUserBook = (req,res) => {
     let bookId = req.body.bookId;
     let userId = req.body.userId
-models.Book.findById(bookId).then((book) => {
+models.Book.findByPk(bookId).then((book) => {
     book.addReader(userId);
 })
 .then(() => {
